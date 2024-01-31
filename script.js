a8b4f7658898bdb36f2482a733b5efdb9b380df7
philype <lellis.sena@aluno.ifsp.edu.br>
2024-01-31 13:00:42 -0300

    Criando cores

---
+++ script.js
@@ -0,0 +1,28 @@
+const verdeCipreste = {
+    nome:'Verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+};
+
+const azulInverno = {
+    nome:'Azul-inverno',
+    pasta: 'imagens-azul-inerno'
+};
+
+const meiaNoite = {
+    nome:'Meia-noite',
+    pasta: 'imagens-meia-noite'
+};
+
+const estelar = {
+    nome:'Estelar',
+    pasta: 'imagens-estelar'
+};
+
+const rosaClaro = {
+    nome:'Rosa-claro',
+    pasta: 'imagens-rosa-claro'
+};
+
+
+// usando array para manipular as variaveis acimas 
+const opcoesCores =[verdeCipreste,azulInverno ,meiaNoite,estelar,rosaClaro]
